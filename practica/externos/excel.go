adb4a8b78f2ceab21ca00479f8ba86fad3a3d6b9
Jesus Alejandro Garcia <garciacode96@gmail.com>
2022-01-04 22:17:54 -0600

    Refactorizacion de codigo

--- practica/externos/excel.go
+++ practica/externos/excel.go
@@ -11,10 +11,9 @@ func main() {
 
 func creacionLibroExcel() {
 	excelFile := excelize.NewFile()
-	excelSheet := excelFile.NewSheet("Main Sheet")
-	excelFile.SetCellValue("Main Sheet", "A1", "Hola, soy una celda hecha en Golang")
-	excelFile.SetActiveSheet(excelSheet)
-	if error := excelFile.SaveAs("ExcelGo.xlsx"); error != nil {
-		fmt.Println(error)
+	excelFile.SetCellValue("Sheet1", "A1", "Hola, soy una celda hecha en Golang")
+	isError := excelFile.SaveAs("ExcelGo.xlsx") // Si existe un error lo retorna
+	if isError != nil {
+		fmt.Println(isError)
 	}
 }
